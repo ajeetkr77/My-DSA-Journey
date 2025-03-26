3bf621b07a3b11e42c132725a86143d063ca2173
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-03-26 10:38:41 +0530

    Update 2033. Minimum Operations to Make a Uni-Value Grid.cpp

--- LeetCode POTD/2033. Minimum Operations to Make a Uni-Value Grid.cpp
+++ LeetCode POTD/2033. Minimum Operations to Make a Uni-Value Grid.cpp
@@ -1,1 +1,28 @@
 
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> flat;
+        int rem = grid[0][0] % x;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                flat.push_back(grid[i][j]);
+                if(grid[i][j] % x != rem) return -1;
+            }
+        }
+
+        sort(flat.begin(), flat.end());
+        int mid = m*n / 2;
+        int minStep = 0;
+
+        for(int i = 0; i < m*n; i++){
+            minStep += abs(flat[i] - flat[mid]) / x;
+        }
+
+        return minStep;
+    }
+};
