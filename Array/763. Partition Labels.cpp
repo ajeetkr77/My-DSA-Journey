dfb8185cb38cc5cf33a6bbe1cc2ddd5cb40f0d20
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-03-30 09:50:39 +0530

    Update 763. Partition Labels.cpp

--- Array/763. Partition Labels.cpp
+++ Array/763. Partition Labels.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> ans;
+        int n = s.length();
+        map<char, int> mp;
+        for(int i = 0; i < n; i++){
+            mp[s[i]] = i;
+        }
+    
+        int i = 0;
+        while(i < n){
+            int start = i;
+            int max_index = mp[s[i]];
+            while(i < max_index){
+                max_index = max(max_index, mp[s[i]]);
+                i++;
+            }
+            i++;
+            ans.push_back(max_index - start + 1);
+        }
 
+        return ans;
+    }
+};
