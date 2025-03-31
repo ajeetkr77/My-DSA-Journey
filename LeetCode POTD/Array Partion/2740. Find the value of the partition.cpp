38c8da5d06a1fb1ba9dc4055b7303e89a37b9550
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-03-31 14:24:20 +0530

    Update 2740. Find the value of the partition.cpp

--- LeetCode POTD/Array Partion/2740. Find the value of the partition.cpp
+++ LeetCode POTD/Array Partion/2740. Find the value of the partition.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int findValueOfPartition(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        // strategy is to arrange nums into 
+        // smallest, 2nd smallest ......bigger
+        // taking difference of adjacents and taking its minimum
+        // will be answer
 
+        int ans = INT_MAX;
+        for(int i = 0; i < nums.size() - 1; i++){
+            ans = min(nums[i+1] - nums[i], ans);
+        }
+        return ans;
+    }
+};
