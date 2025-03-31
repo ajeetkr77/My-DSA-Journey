501760f121ed443fb5bdc5db05818e37a93c59c0
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-03-31 13:46:01 +0530

    Update 561. array partition.cpp

--- LeetCode POTD/Array Partion/561. array partition.cpp
+++ LeetCode POTD/Array Partion/561. array partition.cpp
@@ -1,1 +1,14 @@
+// LINK :  LeetCode 561 : https://leetcode.com/problems/array-partition/description/
 
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+
+        for(int i = 0; i < nums.size(); i+=2){
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
