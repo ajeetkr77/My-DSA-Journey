9aaf2b7b1183837c192c5f3dfccd1a66b85cf716
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-03-31 14:07:21 +0530

    Update 1877. Minimize maximum pair sum in array.cpp

--- LeetCode POTD/Array Partion/1877. Minimize maximum pair sum in array.cpp
+++ LeetCode POTD/Array Partion/1877. Minimize maximum pair sum in array.cpp
@@ -1,1 +1,12 @@
 
+// LeetCode 1877 : https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int ans = nums[0] + nums[n-1];
+        for(int i = 1;  i < n/2; i++) ans = max(ans, nums[i] + nums[n-1-i]);
+        return ans;
+    }
+};
