80b888153b4a077daad3aa5ecd3109e25910fb16
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-02 10:17:34 +0530

    Update 2908. Minimum sum of mountain triplet I.cpp

--- LeetCode POTD/Array/April 2/2908. Minimum sum of mountain triplet I.cpp
+++ LeetCode POTD/Array/April 2/2908. Minimum sum of mountain triplet I.cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+    int minimumSum(vector<int>& nums) {
+        int n = nums.size();
+        int sum = INT_MAX;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(nums[j] > nums[i] ){
+                    for(int k = j+1; k < n; k++){
+                        if(nums[k] < nums[j]){
+                            sum = min(sum, nums[i] + nums[j] + nums[k]);
+                        }    
+                    }
+                }
+            }
+        }
+        return (sum == INT_MAX) ? -1 : sum;
+    }
+};
