db5025c273f414e22d0a105e9d9564a71d29703a
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-02 09:57:49 +0530

    Update 2873. maximum value of an ordered triplet i.cpp

--- LeetCode POTD/Array/2873. maximum value of an ordered triplet i.cpp
+++ LeetCode POTD/Array/2873. maximum value of an ordered triplet i.cpp
@@ -1,1 +1,19 @@
+*************** Naive Approach **************************
+  Time Complexity : O(N^3), All test cases passed because of 
+small constraints on n 
+***************************************************************
 
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                for(int k = j+1; k < n; k++)
+                    ans = max(ans, (long long)(nums[i] - nums[j])*nums[k]);
+            }
+        }
+        return ans;
+    }
+};
