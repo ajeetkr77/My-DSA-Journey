8487b783dd00521ba32c86535c5504a0e0a7e78a
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-03 18:46:35 +0530

    Update 560. Subarray Sum Equals K.cpp

--- LeetCode POTD/Array/3 April/560. Subarray Sum Equals K.cpp
+++ LeetCode POTD/Array/3 April/560. Subarray Sum Equals K.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int currSum = 0;
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int count = 0;
 
+        for(int i = 0; i < n; i++){
+            currSum += nums[i];
+            if(mp.find(currSum - k) != mp.end())
+                count += mp[currSum - k];                                     
+            mp[currSum]++;
+        }
+        return count;
+    }
+};
