24f84d1be422036c65a2fb13c18bb3b651dd7071
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-04 11:56:26 +0530

    Update 1123. Lowest Common Ancestor of Deepest Leaves.cpp

--- LeetCode POTD/Binary Tree/1123. Lowest Common Ancestor of Deepest Leaves.cpp
+++ LeetCode POTD/Binary Tree/1123. Lowest Common Ancestor of Deepest Leaves.cpp
@@ -1,1 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<TreeNode*, int> find(TreeNode* root){
+        if(!root) return{nullptr, 0};
 
+        pair<TreeNode*, int> left = find(root->left);
+        pair<TreeNode*, int> right = find(root->right);
+
+        if(left.second == right.second)
+            return {root, 1 + left.second};
+        else if(left.second < right.second)
+            return {right.first, 1 + right.second};
+        else 
+            return {left.first, 1 + left.second};
+    }
+
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        pair<TreeNode*, int> res = find(root);
+        return res.first;
+    }
+};
