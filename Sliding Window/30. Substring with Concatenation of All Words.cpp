ba627f3539e29bb7751d2565fb4d3814be36f717
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-04 14:14:24 +0530

    Update 30. Substring with Concatenation of All Words.cpp

--- Sliding Window/30. Substring with Concatenation of All Words.cpp
+++ Sliding Window/30. Substring with Concatenation of All Words.cpp
@@ -1,1 +1,34 @@
+****************************** Brute Force Approach ***************************
+  T. C = O(N^2 logN)
+  S. C = O(N)
+******************************************************************************
 
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int n = s.length();
+        int wordCount = words.size();
+        int wordLen = words[0].length();
+        int windowLen = wordCount * wordLen;
+
+        sort(words.begin(), words.end());
+
+        vector<int> index;
+
+        for(int i = 0; i < n - windowLen + 1;  i++){
+            string temp = s.substr(i, windowLen);
+            //cout<<temp<<endl;
+            int j = 0;
+            vector<string> tempWords;
+            while(j < windowLen){
+                string temp1 = temp.substr(j, wordLen);
+                //cout<<temp1<<endl;
+                tempWords.push_back(temp1);
+                j += wordLen;
+            }
+            sort(tempWords.begin(), tempWords.end());
+            if(words == tempWords) index.push_back(i);
+        }
+        return index;
+    }
+};
