5b6e789e1d5dfa15ba457304ca97d99cd61d8cfd
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-05 17:12:22 +0530

    Update 1863. Sum of All Subset XOR Totals.cpp

--- LeetCode POTD/BackTracking/1863. Sum of All Subset XOR Totals.cpp
+++ LeetCode POTD/BackTracking/1863. Sum of All Subset XOR Totals.cpp
@@ -1,1 +1,69 @@
 
+********************************** Approach 1 ********************
+  Using Recursion 
+
+  class Solution {
+public:
+    int ans = 0;
+    void findAllSubsets(vector<int>& nums, int i, int n, vector<int> &temp){
+        if(i >= n) {
+            int x = 0;
+            for(int k = 0; k < temp.size(); k++)
+                x ^= temp[k];
+            ans += x;
+            return;
+        }
+
+        temp.push_back(nums[i]);
+        findAllSubsets(nums, i+1, n, temp);
+        temp.pop_back();
+        findAllSubsets(nums, i+1, n, temp);
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> temp;
+        findAllSubsets(nums, 0, n, temp);
+        return ans;
+    }
+};
+
+************************************** Approach 2 ****************************
+
+class Solution {
+public:
+    int ans = 0;
+    void findAllSubsets(vector<int>& nums, int i, int n, int x){
+        if(i >= n) {
+            ans += x;
+            return;
+        }
+        x ^= nums[i];
+        findAllSubsets(nums, i+1, n, x);
+        x ^= nums[i];
+        findAllSubsets(nums, i+1, n, x);
+    }
+
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        findAllSubsets(nums, 0, n, 0);
+        return ans;
+    }
+};
+
+******************************** Approach 3 **********************************
+
+  Observation Based 
+******************************************************************************
+  
+  class Solution {
+public:
+
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for(int i = 0; i < n; i++) ans |= nums[i];
+        ans = ans << (n-1);
+        return ans;
+    }
+};
