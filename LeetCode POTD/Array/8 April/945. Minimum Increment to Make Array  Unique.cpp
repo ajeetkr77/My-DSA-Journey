2c9cdd7a7f9df0fd12daa086b08d8dfea410bb18
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-08 12:02:21 +0530

    Update 945. Minimum Increment to Make Array  Unique.cpp

--- LeetCode POTD/Array/8 April/945. Minimum Increment to Make Array  Unique.cpp
+++ LeetCode POTD/Array/8 April/945. Minimum Increment to Make Array  Unique.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int minOp = 0;
+        for(int i = 1; i < n; i++){
+            if(nums[i] <= nums[i-1]){
+                minOp += (nums[i-1] - nums[i] + 1);
+                nums[i] += nums[i-1] - nums[i] + 1;
+            }
+        }
+        for(int i = 0; i < n; i++) cout<<nums[i]<<" ";
+        return minOp;
+    }
+};
