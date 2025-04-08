d20bc0c1326f1f5fe8b70eed4ad58f9b46a3f4eb
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-08 11:19:02 +0530

    Update 3396. Minimum number of Operations to make  Elements in array distinct.cpp

--- LeetCode POTD/Array/8 April/3396. Minimum number of Operations to make  Elements in array distinct.cpp
+++ LeetCode POTD/Array/8 April/3396. Minimum number of Operations to make  Elements in array distinct.cpp
@@ -1,1 +1,30 @@
 
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int minOp = 0;
+        while(true){
+            int n = nums.size();
+            bool isDistinct = true;
+            unordered_map<int, int> mp;
+            for(int i = 0; i < n; i++){
+                if(mp[nums[i]] > 0){
+                    isDistinct = false;
+                    break;
+                }
+                mp[nums[i]]++;
+            }
+
+            if(isDistinct) break;
+
+            if(nums.size() > 3)
+                nums.erase(nums.begin(), nums.begin() + 3);
+            else
+                nums.erase(nums.begin(), nums.end());
+            // for(int i = 0; i <nums.size(); i++) cout<<nums[i]<<" ";
+            // cout<<endl;
+            minOp++;
+        }
+        return minOp;
+    }
+};
