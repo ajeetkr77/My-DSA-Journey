7db1b9357d8fb6f1c833ee4df76864eb08a51fd7
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-09 11:26:30 +0530

    Update 3375. Minimum Operations to Make Array Values Equal to K.cpp

--- LeetCode POTD/Array/9. April/3375. Minimum Operations to Make Array Values Equal to K.cpp
+++ LeetCode POTD/Array/9. April/3375. Minimum Operations to Make Array Values Equal to K.cpp
@@ -1,1 +1,38 @@
 
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        int minOp = 0;
+        
+        int Min = INT_MAX;
+        for(int i = 0; i < n; i++) Min = min(Min, nums[i]);
+        if( Min < k ) return -1;
+
+        set<int> st;
+        for(int i = 0; i < n; i++) st.insert(nums[i]);
+
+        if( k < Min ) return st.size();
+        return st.size() - 1;
+    }
+};
+
+**************** Space Optimization *********************
+
+  class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        int minOp = 0;
+        
+        int Min = INT_MAX;
+        for(int i = 0; i < n; i++) Min = min(Min, nums[i]);
+        if( Min < k ) return -1;
+
+        unordered_set<int> st(nums.begin(), nums.end());
+        //for(int i = 0; i < n; i++) st.insert(nums[i]);
+
+        if( k < Min ) return st.size();
+        return st.size() - 1;
+    }
+};
