3c404442371e5bc61dfa2a2dbf991b5d772f2b44
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-16 17:26:39 +0530

    Update 2537. Count the number of good Subarray.cpp

--- LeetCode POTD/Sliding Window/2537. Count the number of good Subarray.cpp
+++ LeetCode POTD/Sliding Window/2537. Count the number of good Subarray.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        int n = nums.size();
+        long long goodSubarrays = 0;
 
+        unordered_map<int, int> mp;
+
+        int pairs = 0;
+
+        int i = 0, j = 0;
+
+        while(j < n){
+
+            pairs += mp[nums[j]];
+            mp[nums[j]]++;
+
+            while(pairs >= k){
+                goodSubarrays += n - j;
+                mp[nums[i]] --;
+                pairs -= mp[nums[i]];
+                i++;
+            }
+            j++;
+        }
+
+        return goodSubarrays;
+    }
+};
