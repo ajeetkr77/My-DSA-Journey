6fe2e5326361e8b884aa1fd70881500abafba0c4
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-20 00:59:33 +0530

    Update 144. Binary Tree Preorder Traversal.cpp

--- Binary Tree/144. Binary Tree Preorder Traversal.cpp
+++ Binary Tree/144. Binary Tree Preorder Traversal.cpp
@@ -1,1 +1,45 @@
+*************************************************************
 
+Iterative Approach : Time Complexity : O(N)
+class Solution {
+public:
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        if(root)
+            st.push(root);
+
+        while(!st.empty()){
+            TreeNode* temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+
+            if(temp->right)
+                st.push(temp->right);
+            if(temp->left)
+                st.push(temp->left);
+        }
+        return ans;
+    }
+};
+
+***********************************************************************
+
+Recursive Approach ; Time complexity = O(N)
+
+class Solution {
+public:
+    void preorder(TreeNode* root, vector<int> &ans){
+        if(root == nullptr) return;
+
+        ans.push_back(root->val);
+        preorder(root->left, ans);
+        preorder(root->right, ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        preorder(root, ans);
+        return ans;
+    }
+};
