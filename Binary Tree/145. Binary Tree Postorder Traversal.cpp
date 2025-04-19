c97f0e4f92447d0f1fb0ea7bfcb44250b79b2e27
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-20 01:35:51 +0530

    Update 145. Binary Tree Postorder Traversal.cpp

--- Binary Tree/145. Binary Tree Postorder Traversal.cpp
+++ Binary Tree/145. Binary Tree Postorder Traversal.cpp
@@ -1,1 +1,57 @@
+****************************************
+  Recursive Approach: O(N)
 
+***************************************
+
+
+class Solution {
+public:
+    void postorder(TreeNode* root, vector<int> &ans){
+        if(root == nullptr)
+            return;
+
+        postorder(root->left, ans);
+        postorder(root->right, ans);
+
+        ans.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        postorder(root, ans);
+
+        return ans;
+    }
+};
+
+****************************************
+  Iterative Approach: O(N)
+
+***************************************
+
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        stack<TreeNode*> st;
+        if(root)
+            st.push(root);
+
+        while(!st.empty()){
+            TreeNode* temp = st.top();
+            ans.push_back(temp->val);
+            st.pop();
+
+            if(temp->left)
+                st.push(temp->left);
+            if(temp->right)
+                st.push(temp->right);
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
