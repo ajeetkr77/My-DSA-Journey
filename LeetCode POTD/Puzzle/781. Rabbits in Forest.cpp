6667e34db1071f2326c3da44f49c5a48521e6bc8
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-20 20:25:13 +0530

    Update 781. Rabbits in Forest.cpp

--- LeetCode POTD/Puzzle/781. Rabbits in Forest.cpp
+++ LeetCode POTD/Puzzle/781. Rabbits in Forest.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        int n = answers.size();
 
+        vector<int> ans(1000, 0);
+
+        int rabbits = 0;
+
+        for(int i = 0; i < n; i++){
+            ans[answers[i]]++;
+        }
+
+        for(int i = 0; i < 1000; i++){
+            if(ans[i]){
+
+                float curr = ans[i] / (float)(i+1);
+
+                int curr1 = ceil(curr);
+                
+                rabbits += (curr1 * (i+1));
+            }
+        }
+        return rabbits;
+    }
+};
