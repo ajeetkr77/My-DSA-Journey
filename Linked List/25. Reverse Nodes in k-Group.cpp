3f6117f5c35b23e0698389116c0a7233623fa6c7
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-21 15:46:43 +0530

    Update 25. Reverse Nodes in k-Group.cpp

--- Linked List/25. Reverse Nodes in k-Group.cpp
+++ Linked List/25. Reverse Nodes in k-Group.cpp
@@ -50,3 +50,80 @@ public:
         return head1;
     }
 };
+
+
+****************************************************************
+
+Time Complexity : O(N^2)
+
+Space Complexity : O(1)
+
+*************************************************************
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        int count=0;
+        ListNode* curr, *headNew=nullptr;
+        ListNode* dummy= new ListNode(-1);
+        dummy->next=head;
+        if(left==right)
+            return head;
+        head=dummy;
+        while(head){
+            if(count==left-1){
+                ListNode *left=head;
+                head=head->next;
+                left->next=nullptr;
+                count++;
+                while(count<=right){
+                    curr=head;
+                    head=head->next;
+                    curr->next=headNew;
+                    headNew=curr;
+                    count++;
+                }
+                left->next=headNew;
+                while(headNew->next!=nullptr){
+                    headNew=headNew->next;
+                }
+                headNew->next=head;
+                break;
+            }
+            count++;
+            head=head->next;
+        }
+        return dummy->next;
+    }
+
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        
+        int n = 0;
+        ListNode* temp = head;
+
+        while(temp != nullptr){
+            n ++;
+            temp = temp->next;
+        }
+        
+        int index = 1; 
+
+        while( index + k - 1 <= n ){
+            head = reverseBetween(head, index, index + k - 1);
+            index = index + k;
+        }
+
+        return head;
+    }
+};
