34c18dd0596c44475ac1fdd91b903e98d3965a9a
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-23 03:24:12 +0530

    Update 62.Unique Paths.cpp

--- DP/62.Unique Paths.cpp
+++ DP/62.Unique Paths.cpp
@@ -1,1 +1,25 @@
+********************************
 
+  Memoization 
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+    Solution(){
+        dp.resize(101, vector<int> (101, -1));
+    }
+
+    int uniquePaths(int m, int n) {
+        if(m < 0 || n < 0) return 0;
+
+        if(m == 1 && n == 1) return 1;
+
+        if(dp[m][n] != -1) return dp[m][n];
+
+        int up = uniquePaths(m-1, n);
+
+        int left = uniquePaths(m, n-1);
+
+        return dp[m][n] = up + left;
+    }
+};
