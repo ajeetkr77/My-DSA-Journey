04f631de825ae3a1db59dcb9c26ee3c03c3340ae
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-23 17:51:27 +0530

    Update 1399. Count Largest Group.cpp

--- LeetCode POTD/Array/1399. Count Largest Group.cpp
+++ LeetCode POTD/Array/1399. Count Largest Group.cpp
@@ -1,1 +1,37 @@
+***********************************
 
+  Naive Approach 
+
+************************************
+
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        map<int, int> mp;
+        int maxSize = 1;
+
+        for(int i = 1; i <= n; i++){
+            int num = i;
+            int sumDigit = 0;
+
+            while(num != 0){
+                sumDigit += num % 10;
+                num /= 10;
+            }
+
+            mp[sumDigit]++;
+
+            maxSize = max(maxSize, mp[sumDigit]);
+        }
+
+        int groups = 0;
+        //cout<<maxSize<<endl;
+
+        for(auto & it : mp){
+            //cout<<it.first<<" "<<it.second<<endl;
+            if(it.second == maxSize) groups++;
+        }
+
+        return groups;
+    }
+};
