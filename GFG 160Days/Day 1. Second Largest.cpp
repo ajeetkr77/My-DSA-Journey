31d5846c817e3881f7cb0ae77b4d46bff8304397
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-24 14:26:26 +0530

    Update Day 1. Second Largest.cpp

--- GFG 160Days/Day 1. Second Largest.cpp
+++ GFG 160Days/Day 1. Second Largest.cpp
@@ -1,3 +1,7 @@
+**********************************************
+  Better Approach : 2 Times Search 
+**********************************************
+
 class Solution {
   public:
     int getSecondLargest(vector<int> &arr) {
@@ -18,5 +22,31 @@ class Solution {
         }
         
         return (maxi2 == INT_MIN) ? -1 : maxi2;
+    }
+};
+
+*************************************************
+Optimal Approach : 1 Time Search
+*************************************************
+
+  class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        
+        int maxi = max(arr[0], arr[1]);
+        int maxi2 = min(arr[0], arr[1]);
+        
+        for(int i  = 2; i < n; i++){
+            if(arr[i] > maxi){
+                maxi2 = maxi;
+                maxi = arr[i];
+            }else if(arr[i] > maxi2 && arr[i] != maxi){
+                maxi2 = arr[i];
+            }
+        }
+        
+        return (maxi2 == maxi) ? -1 : maxi2;
     }
 };
