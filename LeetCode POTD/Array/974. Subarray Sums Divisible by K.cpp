17aa93faaecc7dda0583c182b3c1173ae9305e6a
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-25 21:43:21 +0530

    Update 974. Subarray Sums Divisible by K.cpp

--- LeetCode POTD/Array/974. Subarray Sums Divisible by K.cpp
+++ LeetCode POTD/Array/974. Subarray Sums Divisible by K.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
 
+        int result = 0;
+
+        map<int, int> mp;
+        mp[0] = 1;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+
+            int r1 = sum % k;
+            r1 += k;
+
+            r1 %= k;
+            
+            if(mp.find(r1) != mp.end()){
+                result += mp[r1];
+            }
+
+            mp[r1]++;
+        }
+
+        return result;
+    }
+};
