4f93ea93939b1e8264bc8c978e8ad40ae7eb0a18
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-25 20:29:51 +0530

    Update 2845. Count of Interesting Subarrays.cpp

--- LeetCode POTD/Array/2845. Count of Interesting Subarrays.cpp
+++ LeetCode POTD/Array/2845. Count of Interesting Subarrays.cpp
@@ -1,1 +1,38 @@
 
+class Solution {
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        int n = nums.size();
+
+        vector<int> mod(n, 0);
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] % modulo == k)
+                mod[i] = 1;
+        }
+
+        map<int, int> mp ;
+        mp[0] = 1;
+
+        int sum = 0;
+
+        long long result = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += mod[i];
+
+            int r1 = sum % modulo;
+            int r2 = r1 - k + modulo;
+            r2 %= modulo;
+
+            if(mp.find(r2) != mp.end()){
+                result += mp[r2];
+            }
+
+            mp[r1]++;
+        }
+
+        return result;
+        
+    }
+};
