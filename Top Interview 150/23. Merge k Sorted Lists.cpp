a3bbc8c10e7820c24be72f90957a98df59d506c1
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-28 20:00:29 +0530

    Update 23. Merge k Sorted Lists.cpp

--- Top Interview 150/23. Merge k Sorted Lists.cpp
+++ Top Interview 150/23. Merge k Sorted Lists.cpp
@@ -1,1 +1,47 @@
+*****************************************
+  Naive Approach
+****************************************
 
+  /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+
+        vector<int> list;
+
+        for(int i = 0; i < n; i++){
+            ListNode* temp = lists[i];
+
+            while(temp != nullptr){
+                list.push_back(temp->val);
+                temp = temp->next;
+            }
+        }
+        
+        if(list.size() == 0) 
+            return nullptr;
+
+        sort(list.begin(), list.end());
+
+        ListNode *head = new ListNode(list[0]);
+
+        ListNode *temp = head;
+
+        for(int i = 1; i < list.size(); i++){
+            temp->next = new ListNode(list[i]);
+            temp = temp->next;
+        }
+
+        return head;
+    }
+};
