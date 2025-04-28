e4dea17283e16f4ea33fbba33b3b28e51a6d2f05
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-28 12:52:02 +0530

    Update 2302. Count Subarrays With Score Less Than K.cpp

--- LeetCode POTD/Sliding Window/2302. Count Subarrays With Score Less Than K.cpp
+++ LeetCode POTD/Sliding Window/2302. Count Subarrays With Score Less Than K.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int n = nums.size();
 
+        long long ans = 0;
+
+        int i = 0, j = 0;
+        long long sum = 0, len = 1;
+
+        while(j < n){
+
+            sum += nums[j];
+
+            len = j - i + 1;
+
+            while(sum * len >= k){
+                sum -= nums[i];
+                i++;
+                len--;
+            }
+            ans += j - i + 1;
+            j++;
+        }
+
+        return ans;
+    }
+};
