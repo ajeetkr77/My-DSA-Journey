45dd71aba700d2a5550ae7e8b56413efee2925bb
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-28 20:43:08 +0530

    Update 23. Merge k Sorted Lists.cpp

--- Top Interview 150/23. Merge k Sorted Lists.cpp
+++ Top Interview 150/23. Merge k Sorted Lists.cpp
@@ -1,5 +1,7 @@
 *****************************************
+  
   Naive Approach
+  
 ****************************************
 
   /**
@@ -45,3 +47,68 @@ public:
         return head;
     }
 };
+
+**********************************************
+
+  Better Approach
+  
+*********************************************
+
+  /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* list1, ListNode* list2){
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                temp->next = list2;
+                list2 = list2->next;
+            }  
+            temp = temp->next; 
+        }
+
+        if(list1)
+            temp->next = list1;
+        
+        if(list2)
+            temp->next = list2;
+
+        return dummy->next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+
+        if(n == 0) return nullptr;
+
+        ListNode* head = lists[0];
+
+        for(int i = 1; i < n; i++){
+            head = merge(head, lists[i]);
+        }
+
+        return head;
+    }
+};
+
+
+********************************************************
+
+  Optimal Approach
+
+********************************************************
