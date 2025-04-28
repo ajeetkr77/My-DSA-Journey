89d77ae4826fef3b540a15bc4bb7bfeaadb29a94
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-28 19:35:36 +0530

    Update Day 5. Next Permutation.cpp

--- GFG 160Days/Day 5. Next Permutation.cpp
+++ GFG 160Days/Day 5. Next Permutation.cpp
@@ -1,1 +1,38 @@
-
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int index = -1;
+        
+        for(int i = n-2; i >= 0; i--){
+            if(arr[i] < arr[i+1]){
+                index = i;
+                break;
+            }
+        }
+        
+        if(index == -1){
+            reverse(arr.begin(), arr.end());
+            return;
+        }
+        
+        int nextIndex = -1;
+        
+        for(int i = index + 1; i < n; i++){
+            if(arr[i] > arr[index]){
+                if(nextIndex == -1) nextIndex = i;
+                else{
+                    if(arr[i] < arr[nextIndex])
+                        nextIndex = i;
+                }
+            }
+        }
+        
+        //cout<<arr[index]<<" "<<arr[nextIndex];
+        swap(arr[index], arr[nextIndex]);
+        
+        reverse(arr.begin() + index + 1, arr.end());
+        
+    }
+};
