31d92e50c50d8084ba90dcd93e241dd3352177b9
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-29 20:41:51 +0530

    Update Day 7. Stock Buy and Sell.cpp

--- GFG 160Days/Day 7. Stock Buy and Sell.cpp
+++ GFG 160Days/Day 7. Stock Buy and Sell.cpp
@@ -1,1 +1,19 @@
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int n = prices.size();
+        
+        int profit = 0;
+        
+        int stock = prices[0];
+        
+        for(int i = 1; i < n; i++){
+            if(prices[i] > stock)
+                profit += (prices[i] - stock);
+            stock = prices[i];
+        }
+        return profit;
+    }
+};
 
