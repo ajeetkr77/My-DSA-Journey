74ba50ff6f9d7422ee6d4751cf343a60f0fb9681
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-29 20:05:10 +0530

    Update Day 6. Majority Element ii.cpp

--- GFG 160Days/Day 6. Majority Element ii.cpp
+++ GFG 160Days/Day 6. Majority Element ii.cpp
@@ -1,1 +1,53 @@
-
+class Solution {
+  public:
+    // Function to find the majority elements in the array
+    vector<int> findMajority(vector<int>& nums) {
+        // Your code goes here.
+        int n = nums.size();
+        
+        int majEle1, majEle2;
+        
+        int count1=0, count2 = 0;
+        
+        // Assumption
+        
+        for(int i = 0; i < n; i++){
+            if(nums[i] == majEle1){
+                count1++;
+            }else if(nums[i] == majEle2){
+                count2++;
+            }else if(count1 == 0){
+                majEle1 = nums[i];
+                count1 = 1;
+            }else if(count2 == 0){
+                majEle2 = nums[i];
+                count2 = 1;
+            }else{
+                count1 --, count2--;
+            }
+        }
+        
+        // Verification
+        
+        count1 = 0, count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == majEle1){
+                count1++;
+            }
+            if(nums[i] == majEle2){
+                count2++;
+            }
+        }
+        
+        vector<int> ans;
+        
+        if(count1 > n / 3) ans.push_back(majEle1);
+        
+        if(count2 > n / 3) ans.push_back(majEle2);
+        
+        sort(ans.begin(), ans.end());
+        
+        return ans;
+        
+    }
+};
