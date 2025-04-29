aa956b55170b81be19e9adb18058cd111aba2d09
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-29 18:47:31 +0530

    Update 2962. Count Subarrays Where Max Element Appears at Least K Times.cpp

--- LeetCode POTD/Sliding Window/2962. Count Subarrays Where Max Element Appears at Least K Times.cpp
+++ LeetCode POTD/Sliding Window/2962. Count Subarrays Where Max Element Appears at Least K Times.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int maxElement = INT_MIN;
 
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) maxElement = max(maxElement, nums[i]);
+
+        long long result = 0;
+
+        int i = 0, j = 0, count = 0;
+
+        while( j < n ){
+
+            if(nums[j] == maxElement) count++;
+
+            while(count >= k){
+                result += n - j;
+                if(nums[i] == maxElement) count--;
+                i++;
+            }
+            
+            j++;
+        }
+
+        return result;
+    }
+};
