e978faf2b177c6839118b2a4dd575a834b40b3d8
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-04-29 21:01:22 +0530

    Update Day 8. Stock Buy and Sell – Max one Transaction Allowed.cpp

--- GFG 160Days/Day 8. Stock Buy and Sell – Max one Transaction Allowed.cpp
+++ GFG 160Days/Day 8. Stock Buy and Sell – Max one Transaction Allowed.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int profit = 0;
+        
+        int n = prices.size();
+        
+        int maxEle = prices[n-1];
+        
+        for(int i = n - 2; i >= 0; i--){
+            
+            if(prices[i] < maxEle){
+                profit = max(profit, maxEle - prices[i]);
+            }
+            
+            maxEle = max(maxEle, prices[i]);
+        }
+        
+        return profit;
+    }
+};
