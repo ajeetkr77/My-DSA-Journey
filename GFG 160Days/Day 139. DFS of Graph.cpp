f8107f762f3013710b047f730bee373bd6686eed
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-05-01 22:27:22 +0530

    Update Day 139. DFS of Graph.cpp

--- GFG 160Days/Day 139. DFS of Graph.cpp
+++ GFG 160Days/Day 139. DFS of Graph.cpp
@@ -1,1 +1,28 @@
 
+class Solution {
+  public:
+    void dfsSearch(vector<vector<int>>& adj, vector<int> &ans, vector<int> &vis,  int u){
+        vis[u] = 1;
+        
+        ans.push_back(u);
+        
+        for(auto it : adj[u]){
+            if(!vis[it]){
+                dfsSearch(adj, ans, vis, it);
+            }
+        }
+    }
+    
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        int n = adj.size();
+        
+        vector<int> vis(n, 0);
+        
+        vector<int> ans;
+        
+        dfsSearch(adj, ans, vis, 0);
+        
+        return ans;
+    }
+};
