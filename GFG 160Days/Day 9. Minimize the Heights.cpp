f7c454c153ceff65af0075fb041e87d2d210edc5
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-05-01 10:57:25 +0530

    Update Day 9. Minimize the Heights.cpp

--- GFG 160Days/Day 9. Minimize the Heights.cpp
+++ GFG 160Days/Day 9. Minimize the Heights.cpp
@@ -1,1 +1,35 @@
+************************************************
 
+  Minimize the Heights I
+
+***********************************************
+
+  
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        
+        sort(arr.begin(), arr.end());
+        
+        
+        int ans = arr[n-1] - arr[0]; // k == 0 
+        
+        int smallest = arr[0] + k;
+        int largest = arr[n-1] - k;
+        
+        int mi , ma;
+        
+        for(int i = 0; i < n-1; i++){
+            
+            mi = min(smallest, arr[i+1] - k);
+            ma = max(largest, arr[i] + k);
+            
+            ans = min(ans, ma - mi);
+        }
+        
+        return ans;
+        
+    }
+};
