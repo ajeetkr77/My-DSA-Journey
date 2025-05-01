606a8d452ec582ee361b34ffad366973658f8625
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-05-01 22:37:06 +0530

    Update Day 139. BFS of graph.cpp

--- GFG 160Days/Day 139. BFS of graph.cpp
+++ GFG 160Days/Day 139. BFS of graph.cpp
@@ -1,1 +1,36 @@
-
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
+        // Code here
+        int n = adj.size();
+        
+        vector<int> vis(n, 0);
+        
+        vector<int> ans;
+        
+        queue<int> q;
+        
+        vis[0] = 1;
+        
+        q.push(0);
+        
+        while(!q.empty()){
+            
+            int u = q.front();
+            
+            ans.push_back(u);
+            
+            q.pop();
+            
+            for(int it : adj[u]){
+                if(!vis[it]){
+                    q.push(it);
+                    vis[it] = 1;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
