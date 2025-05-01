ee76db4213816d7eeb2a2a5c3c53971c5537de91
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-05-01 10:06:35 +0530

    Update Day 11. Maximum Product Subarray.cpp

--- GFG 160Days/Day 11. Maximum Product Subarray.cpp
+++ GFG 160Days/Day 11. Maximum Product Subarray.cpp
@@ -1,1 +1,29 @@
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        // Your Code Here
+        int n = arr.size();
+        
+        int prefixMul = 1, suffixMul = 1, maxProduct = INT_MIN;
+        
+        for(int i = 0; i < n; i++){
+            
+            prefixMul *= arr[i];
+            maxProduct = max(maxProduct, prefixMul);
+            
+            if(prefixMul == 0) prefixMul = 1;
+        }
+        
+        for(int i = n-1; i >= 0; i--){
+            
+            suffixMul *= arr[i];
+            maxProduct = max(maxProduct, suffixMul);
+            
+            if(suffixMul== 0) suffixMul = 1;
+        }
+        
+        return maxProduct;
+    }
+};
 
