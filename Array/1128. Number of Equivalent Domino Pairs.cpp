a98f39fd17cf47bd264ea25876d4c0bbe5dbf296
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-05-04 13:33:09 +0530

    Update 1128. Number of Equivalent Domino Pairs.cpp

--- Array/1128. Number of Equivalent Domino Pairs.cpp
+++ Array/1128. Number of Equivalent Domino Pairs.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    int findCombination(int n){
 
+        if( n == 2 ) return 1; 
+
+        return (n * (n-1)) / 2;
+
+    }
+
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        
+        int n = dominoes.size();
+
+        map<pair<int, int>, int> mp;
+
+        for(int i = 0; i < n; i++){
+            mp[{min(dominoes[i][0], dominoes[i][1]), max(dominoes[i][0], dominoes[i][1])}]++;
+        }
+
+        int ans = 0;
+        for(auto& u : mp){
+            //cout<<u.second<<" ";
+            if(u.second >= 2){
+                ans += findCombination(u.second);
+            }
+        }
+
+        return ans;
+    }
+};
