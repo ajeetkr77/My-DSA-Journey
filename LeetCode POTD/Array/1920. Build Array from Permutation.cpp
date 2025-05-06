d9c7288350661b653c2849c110ec7cf5f8336f8d
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-05-06 23:07:53 +0530

    Update 1920. Build Array from Permutation.cpp

--- LeetCode POTD/Array/1920. Build Array from Permutation.cpp
+++ LeetCode POTD/Array/1920. Build Array from Permutation.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans(n);
 
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+};
+
+*****************************************************
+              Using Constant Space
+*****************************************************
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            nums[i] += n*(nums[nums[i]] % n);
+        }
+
+        for(int i = 0; i < n; i++){
+            nums[i] /= n;
+        }
+
+        return nums;
+    }
+};
