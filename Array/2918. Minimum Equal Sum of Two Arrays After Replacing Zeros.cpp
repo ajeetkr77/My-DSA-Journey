c8b8d988dc822362b1486f17c5e4bb29a2f6902e
ajeetkr77 <44709276+ajeetkr77@users.noreply.github.com>
2025-05-10 09:20:43 +0530

    Update 2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp

--- Array/2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp
+++ Array/2918. Minimum Equal Sum of Two Arrays After Replacing Zeros.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        
+        long long sum1 = 0, sum2 = 0, count1 = 0, count2 = 0;
 
+        for(int i = 0; i < nums1.size(); i++){
+            sum1 += nums1[i];
+            if(nums1[i] == 0) count1++;
+        }
+
+        for(int i = 0; i < nums2.size(); i++){
+            sum2 += nums2[i];
+            if(nums2[i] == 0) count2++;
+        }
+
+        if(sum1 + count1 == sum2 + count2) return sum1 + count1;
+
+        if(count1 != 0 && count2 != 0){
+            return max(sum1 + count1 , sum2 + count2);
+        }
+
+        if(count1 != 0 && count2 == 0){
+            if(sum2 >= sum1 + count1) return sum2;
+            else 
+                return -1;
+        }
+
+        if(count2 != 0 && count1 == 0){
+            if(sum1 >= sum2 + count2) return sum1;
+            else 
+                return -1;
+        }
+
+        return -1;
+    }
+};
