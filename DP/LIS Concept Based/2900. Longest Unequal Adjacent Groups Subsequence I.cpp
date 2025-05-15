68d36095d6e0254753780d58e5da2b16c596c6ad
Ajeet Kumar <44709276+ajeetkr77@users.noreply.github.com>
2025-05-15 09:53:01 +0530

    Create 2900. Longest Unequal Adjacent Groups Subsequence I.cpp

---
+++ DP/LIS Concept Based/2900. Longest Unequal Adjacent Groups Subsequence I.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        
+        int n = groups.size();
+
+        vector<int> dp(n, 1), parent(n);
+
+        for(int i = 0; i < n; i++) parent[i] = i;
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+
+                if(groups[i] != groups[j] && dp[i] < 1 + dp[j]){
+                    parent[i] = j;
+                    dp[i] = 1 + dp[j];
+                }
+            }
+        }
+
+        vector<string> ans;
+
+        int index = 0, maxi = dp[0];
+
+        for(int i = 1; i < n; i++){
+            if(maxi < dp[i]){
+                maxi = dp[i];
+                index = i;
+            }
+        }
+
+        while(parent[index] != index){
+            ans.push_back(words[index]);
+            index = parent[index];
+        }
+
+        ans.push_back(words[index]);
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
