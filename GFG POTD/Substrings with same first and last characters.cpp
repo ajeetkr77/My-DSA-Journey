79429fac9e59a6fbe09c18a977a1cd75490f3d23
Ajeet Kumar <44709276+ajeetkr77@users.noreply.github.com>
2025-05-15 10:10:07 +0530

    Update Substrings with same first and last characters.cpp

--- GFG POTD/Substrings with same first and last characters.cpp
+++ GFG POTD/Substrings with same first and last characters.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+  public:
+    int countSubstring(string &s) {
+        // code here
+        vector<int> fre(26, 0);
+        
+        for(char ch : s) fre[ch - 'a']++;
+        
+        int ans = 0; 
+        
+        for(int i = 0; i < 26; i++){
+            if(fre[i] > 0){
+                
+                int extra = (fre[i] < 2) ? 0 : (fre[i] * (fre[i] - 1)) / 2;
+                
+                ans += extra + fre[i];
+                
+            }
+        }
+        
+        return ans;
+    }
+};
