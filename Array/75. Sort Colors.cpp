509e5995060367ee512263e5034996b40a4f22fa
Ajeet Kumar <44709276+ajeetkr77@users.noreply.github.com>
2025-05-17 13:57:18 +0530

    Update 75. Sort Colors.cpp

--- Array/75. Sort Colors.cpp
+++ Array/75. Sort Colors.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int index = 0;
 
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0){
+                swap(nums[i], nums[index]);
+                index++;
+            }
+        }
+
+        index = n - 1;
+        for(int i = n - 1; i >= 0; i--){
+            if(nums[i] == 2){
+                swap(nums[i], nums[index]);
+                index--;
+            }
+        }
+
+        return;
+    }
+};
