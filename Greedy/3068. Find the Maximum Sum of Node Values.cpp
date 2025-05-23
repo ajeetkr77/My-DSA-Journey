6f323ae68392318a987d41c6e27bbe102e4e2367
Ajeet Kumar <44709276+ajeetkr77@users.noreply.github.com>
2025-05-23 13:40:34 +0530

    Update 3068. Find the Maximum Sum of Node Values.cpp

--- Greedy/3068. Find the Maximum Sum of Node Values.cpp
+++ Greedy/3068. Find the Maximum Sum of Node Values.cpp
@@ -26,3 +26,36 @@ public:
         return sum - minLoss;
     }
 };
+
+***************************************************
+Approach - 2 
+
+***************************************************
+
+    class Solution {
+public:
+    typedef long long ll;
+
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        
+        ll sum = 0;
+        int n = nums.size();
+        
+        vector<int> profit;
+
+        for(int num : nums){
+            sum += num;
+            profit.push_back((num ^ k) - num);
+        }
+
+        sort(profit.rbegin(), profit.rend());
+
+        // chooosing profitable pairs as we can choose only in pairs 
+
+        for(int i = 0; i < n  - 1; i+=2){
+            if(profit[i] + profit[i+1] > 0) sum += profit[i] + profit[i+1];
+        }
+
+        return sum;
+    }
+};
