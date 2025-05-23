e2adb3858a28d3454fd8fc0660d623878ea11961
Ajeet Kumar <44709276+ajeetkr77@users.noreply.github.com>
2025-05-23 16:18:03 +0530

    Update 1143. Longest Common Subsequence.cpp

--- DP/LCS Based Problems/1143. Longest Common Subsequence.cpp
+++ DP/LCS Based Problems/1143. Longest Common Subsequence.cpp
@@ -1,3 +1,47 @@
+*************************************************
+    Recursion + Memoization 
+*************************************************
+class Solution {
+  public:
+    int m, n;
+
+    int t[1001][1001];
+    int solve(string &s1, int i, string &s2, int j){
+        
+        if(i >= m || j >= n) return 0;
+        
+        if(t[i][j] != -1) return t[i][j];
+        
+        int chooseBoth = 0, chooseS1 = 0, chooseS2 = 0;
+        
+        if(s1[i] == s2[j]){
+            chooseBoth = 1 + solve(s1, i + 1, s2, j + 1);
+        }else{
+            chooseS1 = solve(s1, i + 1, s2, j);
+            chooseS2 = solve(s1, i, s2, j + 1);
+        }
+        
+        return t[i][j] = max(chooseBoth, max(chooseS1, chooseS2));
+    }
+    
+    int lcs(string &s1, string &s2) {
+        // code here
+        m = s1.length(), n = s2.length();
+        
+        memset(t, -1, sizeof(t));
+        
+        int ans = solve(s1, 0, s2, 0);
+        
+        
+        
+        return ans;
+    }
+};
+
+
+*************************************************
+    Bottom Up Approach
+*************************************************
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
