fb88a8065651d1c525b52edc33e032bc277e4503
Ajeet Kumar <44709276+ajeetkr77@users.noreply.github.com>
2025-05-27 13:17:54 +0530

    Update 1614. Maximum Nesting Depth of the Parentheses.cpp

--- String/1614. Maximum Nesting Depth of the Parentheses.cpp
+++ String/1614. Maximum Nesting Depth of the Parentheses.cpp
@@ -19,3 +19,29 @@ public:
         return ans;
     }
 };
+
+**************************************************
+using constant space
+**************************************************
+
+    class Solution {
+public:
+    int maxDepth(string s) {
+        
+        int ans = 0;
+        //stack<char> st;
+        int n = s.length();
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '(') count++;
+
+            ans = max(ans, count);
+
+            if(s[i] == ')') count--;
+        }
+
+        return ans;
+    }
+};
