a3b0c76c68e991158ae7f5b8621db7aebe05c159
Ajeet Kumar <44709276+ajeetkr77@users.noreply.github.com>
2025-05-31 15:53:57 +0530

    Create Kth element in Matrix.cpp

---
+++ Heap/Kth element in Matrix.cpp
@@ -0,0 +1,29 @@
+********************************************
+  Naive Approach : using Pq 
+  T C : O(n^2 logn)
+********************************************
+
+class Solution {
+  public:
+    int kthSmallest(vector<vector<int>> &matrix, int k) {
+        // code here
+        int n = matrix.size();
+        
+        priority_queue<int> pq;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                pq.push(matrix[i][j]);
+                
+                if(pq.size() > k) pq.pop();
+            }
+        }
+        
+        return pq.top();
+    }
+};
+
+********************************************
+  Naive Approach : using Pq 
+  T C : O(n^2 logn)
+********************************************
